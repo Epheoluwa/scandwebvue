20e5850f7423dc66611d8e36316f963ab224e6b7
Epheoluwa <sunmolasolomon@gmail.com>
2022-07-22 16:00:03 +0100

    update

--- src/components/AddProduct.vue
+++ src/components/AddProduct.vue
@@ -2,13 +2,29 @@
   <div class="hello">
     <h1>{{ msg }}</h1>
     <div class="container">
-        <div class="row mb-3">
-          <label for="colFormLabel" class="col-sm-2 col-form-label">Email</label>
-          <div class="col-sm-10">
-            <input type="email" class="form-control" id="colFormLabel" placeholder="col-form-label">
+      <form class="row g-3">
+          <div class="col-md-6">
+            <div class="row mb-3">
+                  <label for="colFormLabel" class="col-sm-2 col-form-label">Email</label>
+                  <div class="col-sm-10">
+                    <input type="email" class="form-control" id="colFormLabel" placeholder="col-form-label">
+                  </div>
+            </div>
           </div>
-        </div>
+          <div class="col-md-4">
+            <div class="row mb-3">
+                  <label for="colFormLabel" class="col-sm-2 col-form-label">State</label>
+                  <div class="col-sm-10">
+                    <select id="inputState" class="form-select">
+                      <option selected>Choose...</option>
+                      <option>...</option>
+                    </select>
+                  </div>
+            </div>
+          </div>
+      </form>
     </div>
+   
   </div>
 </template>
 
