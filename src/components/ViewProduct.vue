475926bf52b698545ef34734b4010974c09125f6
Epheoluwa <sunmolasolomon@gmail.com>
2022-08-04 08:32:36 +0100

    update product adding

--- src/components/ViewProduct.vue
+++ src/components/ViewProduct.vue
@@ -5,7 +5,9 @@
   </MainNav>
   <div class="container">
     <div class="card" style="width: 18rem;">
+    
         <div class="card-body">
+          <input type="checkbox" name="" id="" class="checkboxstyle"/>
           <h5 class="card-title">Card title</h5>
           <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
           <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
@@ -18,14 +20,29 @@
 
 <script>
 import MainNav from './mainNav.vue'
+import { onMounted, ref } from 'vue';
+import axios from 'axios';
 export default {
+  components: { MainNav, },
+  
     setup() {
-        return {};
-    },
-    components: { MainNav, }
+      const datavalue = ref([]);
+      const fetchdata = async() =>{
+        let response = await axios.get('http://localhost/phpcrudapi/api/getproduct.php')
+        datavalue.value = response.data;
+      }
+      onMounted(fetchdata);
+      console.log(datavalue);
+        return {
+          datavalue
+          };
+    }
+    
 }
 </script>
 
-<style lang="scss" scoped>
-
+<style scoped>
+.checkboxstyle{
+  margin-right: 80%;
+}
 </style>
